d3488d9e10b9495b34ca7920f1fc73452bcee645
abuzeid <abuzeid7@gmail.com>
2020-12-17 22:22:13 +0100

    Add dev pods workspace

--- DevPodsExample/Pods/Target Support Files/Pods-DevPodsExample-DevPodsExampleUITests/Pods-DevPodsExample-DevPodsExampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DevPodsExample_DevPodsExampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DevPodsExample_DevPodsExampleUITests
-@end
