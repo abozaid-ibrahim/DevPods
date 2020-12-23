370bdfe36605d8ba115cca9ae2cab263eff7e656
abuzeid <Abuzeid.ibrahim@asc-me.net>
2020-12-23 20:52:12 +0100

    Move pagination code to network dev lib

--- Pageable.swift
+++ Pageable.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+public protocol Pageable: class {
+    var page: Page { get }
+    func loadData(_ completion: ((Int) -> Void)?)
+    func loadNewPage(for indexes: [Int])
+}
+
+public extension Pageable {
+    func loadNewPage(for indexes: [Int]) {
+        DispatchQueue.global(qos: .utility).async { [weak self] in
+            guard let self = self else { return }
+            if self.page.fetching { return }
+            self.page.lock.lock()
+            guard let nextRow = indexes.max(),
+                  self.page.shouldLoadMore(for: nextRow) else {
+                self.page.lock.unlock()
+                return
+            }
+            self.page.fetching = true
+            self.loadData {
+                if $0 > 0 { self.page.newPageFetched() }
+                self.page.fetching = false
+                self.page.lock.unlock()
+            }
+        }
+    }
+}
